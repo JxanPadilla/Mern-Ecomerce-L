f0471889c48657e0aa0e63ca250896b2f0bb9b59
JxanPadilla <juanlm101120@gmail.com>
2022-11-22 14:38:47 -0500

    Productos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 // Context
 import { AuthProvider } from './context/AuthProvider';
 import { UsuariosProvider } from './context/UsuariosProvider'
-
+import { ProductosProvider } from './context/ProductosProvider'
 // Layout (LayoutAuth)
 import LayoutAuth from './Layout/LayoutAuth'
 import RutaProtegida from './Layout/RutaProtegida'
@@ -15,13 +15,15 @@ import Confirmar from './pages/usuario/Confirmar'
 import Perfil from './pages/usuario/Perfil'
 import CambiarPassword from './pages/usuario/CambiarPassword.jsx'
 // pages (productos)
-
+import ListaProductos from './pages/productos/ListaProductos'
+import FormularioProductos from './pages/productos/FormularioProductos'
+import DetalleProducto from './pages/productos/DetalleProducto'
 function App() {
     return (
         <Router>
             <AuthProvider>
                 <UsuariosProvider>
-                   
+                    <ProductosProvider>
                         <Routes>
                             {/* RUTAS PUBLICAS */}
                             <Route path='/' element={<LayoutAuth />}>
@@ -35,8 +37,13 @@ function App() {
                                 <Route index element={<Perfil />} />
                                 <Route path="cambiar-password" element={<CambiarPassword />} />
                             </Route>
+                            <Route path='/productos' element={<RutaProtegida />}>
+                                <Route index element={<ListaProductos />} />
+                                <Route path='agregar-producto' element={<FormularioProductos />} />
+                                <Route path='detalle-producto/:id' element={<DetalleProducto />} />
+                            </Route>
                         </Routes>
-                   
+                    </ProductosProvider>
                 </UsuariosProvider>
             </AuthProvider>
         </Router>
